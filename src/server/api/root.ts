945c11df9f00f2b39ea4e83d7d793fa0f18bfed2
Siddharth Kundu <sk31395@gmail.com>
2026-03-03 00:06:00 -0500

    trpc integration and react query setup

---
+++ src/server/api/root.ts
@@ -0,0 +1,10 @@
+import { createCallerFactory, createTRPCRouter, publicProcedure } from "./trpc";
+
+export const appRouter = createTRPCRouter({
+  health: publicProcedure.query(() => {
+    return { status: "ok", timestamps: Date.now() };
+  }),
+});
+
+export type AppRouter = typeof appRouter;
+export const createCaller = createCallerFactory(appRouter);
