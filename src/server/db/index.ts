5f7a46256ec3252997eec031f29a3857e8171b3f
Siddharth Kundu <sk31395@gmail.com>
2026-03-03 21:09:11 -0500

    update prisma to  v7. fix current breaking changes

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const createPrismaClient = () => {
-  return new PrismaClient();
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  });
+  return new PrismaClient({ adapter });
 };
 
 const globalPrismaClient = globalThis as unknown as {
